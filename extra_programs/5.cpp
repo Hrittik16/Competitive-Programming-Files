bb1f048fd328b8de5f0eee3ede584aa9746406d9
Hrittik Das <hrittikdas16@gmail.com>
2019-11-02 20:14:56 +0530

    extra_files updated

---
+++ extra_programs/5.cpp
@@ -0,0 +1,25 @@
+// Problem link : https://codeforces.com/contest/688/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define rep(i, a, b) for(int i = a; i < b; i++) 
+#define repll(i, a, b) for(ll i = a; i < b; i++) 
+#define all(a) (a).begin(),(a).end()
+#define pb push_back
+
+int main() {
+	
+	string n;
+	cin >> n;
+	deque<char> dq;
+	for(int i = n.length()-1; i >= 0; i--) {
+		dq.push_front(n[i]);
+		dq.push_back(n[i]);
+	}
+	for(auto x: dq) cout << x;
+	cout << "\n";
+	return 0;
+}
