c4f499b1f81250906e2021d7ccc6a9c6288170f2
hrittik16 <hrittikdas16@gmail.com>
2019-12-06 22:49:12 +0530

    Updated

--- extra_programs/17.cpp
+++ extra_programs/17.cpp
@@ -11,70 +11,70 @@ using namespace std;
 #define pb push_back
 
 int main() {
-	
 	int n;
 	cin >> n;
 	while(n--) {
-		int count_x = 0, count_o = 0, win_x = 0, win_o = 0, flag = 0;
-		char ch;
-		string s;
+		vector<vector<char>> grid(3, vector<char>(3));
+		int count_x = 0, count_o = 0, winX = 0, winO = 0;
 		rep(i, 0, 3) {
 			rep(j, 0, 3) {
-				cin >> ch;
-				s += ch;
-				if(ch == 'X') count_x++;
-				else if(ch == 'O') count_o++;
+				cin >> grid[i][j];
+				if(grid[i][j] == 'x') 
+					grid[i][j] = toupper(grid[i][j]);
+				if(grid[i][j] == 'o')
+					grid[i][j] = toupper(grid[i][j]);
+				if(grid[i][j] == 'X')
+					count_x++;
+				if(grid[i][j] == 'O')
+					count_o++;
 			}
 		}
-		//cout << "s = " << s << "\n";
-		if(s[4] != '.') {	
-			if(s[4] == 'X') {
-				if((s[0] == 'X' && s[8] == 'X') || (s[1] == 'X' && s[7] == 'X') || (s[2] == 'X' && s[6] == 'X') || (s[3] == 'X' && s[5] == 'X'))
-					win_x = 1;
+				
+		if(grid[1][1] != '.') {
+			if(grid[1][1] == 'X') {
+				if((grid[0][0] == 'X' && grid[2][2] == 'X') || (grid[0][2] == 'X' && grid[2][0] == 'X'))
+					winX = 1;
+				else if((grid[1][0] == 'X' && grid[1][2] == 'X') || (grid[0][1] == 'X' && grid[2][1] == 'X'))
+					winX = 1;
 			}
-			else {
-				if((s[0] == 'O' && s[8] == 'O') || (s[1] == 'O' && s[7] == 'O') || (s[2] == 'O' && s[6] == 'O') || (s[3] == 'O' && s[5] == 'O'))
-					win_o = 1;
-			}
-		}
-		if(s[0] != '.') {
-			if(s[0] == 'X') {
-				if((s[1] == 'X' && s[2] == 'X') || (s[3] == 'X' && s[6] == 'X'))
-					win_x = 1;
-			}
-			else {
-				if((s[1] == 'O' && s[2] == 'O') || (s[3] == 'O' && s[6] == 'O'))
-					win_o = 1;	
+			else {				
+				if((grid[0][0] == 'O' && grid[2][2] == 'O') || (grid[0][2] == 'O' && grid[2][0] == 'O'))
+					winO = 1;
+				else if((grid[1][0] == 'O' && grid[1][2] == 'O') || (grid[0][1] == 'O' && grid[2][1] == 'O'))
+					winO = 1;
 			}
 		}
-		if(s[8] != '.') {
-			if(s[8] == 'X') {
-				if((s[2] == 'X' && s[5] == 'X') || (s[6] == 'X' && s[7] == 'X'))
-					win_x = 1;
+		if(grid[0][0] != '.') {
+			if(grid[0][0] == 'X') {
+				if((grid[0][1] == 'X' && grid[0][2] == 'X') || (grid[1][0] == 'X' && grid[2][0] == 'X'))
+					winX = 1;
 			}
-			else {
-				if((s[2] == 'O' && s[5] == 'O') || (s[6] == 'O' && s[7] == 'O'))
-					win_o = 1;
+			else {				
+				if((grid[0][1] == 'O' && grid[0][2] == 'O') || (grid[1][0] == 'O' && grid[2][0] == 'O'))
+					winO = 1;
 			}
 		}
-
-		if(win_x == 1 && win_o == 1) flag = 1;
-		else {
-			if(win_x == 1 && win_o == 0 && count_x != count_o+1)
-				flag = 1;
-			else if(win_x == 0 && win_o == 1 && count_x != count_o)
-				flag = 1;
-			else if(count_x != count_o+1 && count_x != count_o)
-				flag = 1;
+		if(grid[2][2] != '.') {
+			if(grid[2][2] == 'X') {
+				if((grid[0][2] == 'X' && grid[1][2] == 'X') || (grid[2][0] == 'X' && grid[2][1] == 'X'))
+					winX = 1;
+			}
+			else {				
+				if((grid[0][2] == 'O' && grid[1][2] == 'O') || (grid[2][0] == 'O' && grid[2][1] == 'O'))
+					winO = 1;
+			}
 		}
-		//cout << "win_x = " << win_x << "\n";
-		//cout << "win_o = " << win_o << "\n";
-		if(flag) cout << "no\n";
+		int flag = 0;
+		if(winX == 1 && winO == 1) flag = 1;
+		else if(winX == 1 && winO == 0 && count_x != count_o+1) flag = 1;
+		else if(winO == 1 && winX == 0 && count_x != count_o) flag = 1;
+		else if(count_x != count_o && count_x != count_o+1) flag = 1;
+		if(flag) cout  << "no\n";
 		else cout << "yes\n";
-
-	}
+	}	
 
 
 
 	return 0;
 }
+
