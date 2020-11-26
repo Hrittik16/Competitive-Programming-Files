0c5522400242d54e1019793859bd8b1e7ed6b068
hrittik16 <hrittikdas16@gmail.com>
2020-11-26 15:29:07 +0530

    Solution to C added

--- codeforces/div3#686/c.cpp
+++ codeforces/div3#686/c.cpp
@@ -13,16 +13,50 @@ int main() {
 	while (t--) {
 		int n;
 		cin >> n;
-		vector<int> fre(n, 0);
-		vector<int> a(n);
+		map<int, vector<int>> index;
 		for (int i = 0; i < n; i++) {
-			cin >> a[i];
-			fre[a[i] - 1]++;
+			int num;
+			cin >> num;
+			index[num].push_back(i);
 		}
-		if (a[0] == a[n - 1]) {
-			int total = fre[a[0] - 1] - 1;
 
+		int mn = 1e9 + 7;
+
+		if (index.size() == 1) {
+			cout << "0\n";
+			continue;
 		}
+
+		for (auto x : index) {
+			int count = 0;
+			if (x.second.size() == 1) {
+				if (x.second[0] == 0 || x.second[0] == n - 1)
+					mn = min(mn, 1);
+				else
+					mn = min(mn, 2);
+				continue;
+			}
+			for (int i = 0; i < x.second.size(); i++) {
+				if (i == 0) {
+					if (x.second[i] >= 1) {
+						count++;
+						continue;
+					}
+				}
+				if (i == x.second.size() - 1) {
+					if (x.second[i] < n - 1)
+						count++;
+				}
+				if (x.second[i] - x.second[i - 1 < 0 ? i : i - 1] > 1) {
+					count++;
+				}
+			}
+
+			mn = min(mn, count);
+		}
+
+		cout << mn << "\n";
+
 	}
 
 
