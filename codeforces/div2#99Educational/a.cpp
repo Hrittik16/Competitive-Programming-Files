484e1304e8277da4bdbe0b0cf7dd3091b188b4cc
hrittik16 <hrittikdas16@gmail.com>
2020-11-30 22:54:49 +0530

    updated

---
+++ codeforces/div2#99Educational/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repll(i, a, b) for(ll i = a; i < b; i++)
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define vl vector<ll>
+
+int main() {
+
+
+
+
+	return 0;
+}
