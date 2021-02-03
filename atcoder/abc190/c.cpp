ec3191014eb402e5406a7c1379d2c72fa142c64a
hrittik16 <hrittikdas16@gmail.com>
2021-02-03 13:55:46 +0530

    Updated

--- atcoder/abc190/c.cpp
+++ atcoder/abc190/c.cpp
@@ -22,9 +22,50 @@ using namespace std;
 #define string_to_int(x)	stoi(x)
 #define int_to_string(x)	to_string(x)
 
+int n, m, k;
+vi vec;
+vector<pii> c, d;
+
+int recurse(vector<pii>& d, int curr) {
+	if (curr >= sz(d)) {
+		set<int> st;
+		for (auto x : vec) st.insert(x);
+		int count = 0;
+		rep(i, 0, m) {
+			auto it = c[i];
+			if (st.find(it.f) != st.end() && st.find(it.s) != st.end()) count++;
+		}
+		return count;
+	}
+	auto it = d[curr];
+	vec.pb(it.f);
+	int rec1 = recurse(d, curr + 1);
+	vec.pop_back();
+	vec.pb(it.s);
+	int rec2 = recurse(d, curr + 1);
+	vec.pop_back();
+	return max(rec1, rec2);
+}
+
 int32_t main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
+	cin >> n >> m;
+	rep(i, 0, m) {
+		int a, b;
+		cin >> a >> b;
+		c.pb({a, b});
+	}
+
+	cin >> k;
+	rep(i, 0, k) {
+		int a, b;
+		cin >> a >> b;
+		d.pb({a, b});
+	}
+
+	int ans = recurse(d, 0);
+	cout << ans << "\n";
 
 	return 0;
 }
